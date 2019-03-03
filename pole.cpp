825c1a95c220c13d10361227026bca9d32ca76a2
Giorgi Gvimradze <natceo@gmail.com>
2019-03-03 03:37:39 +0300

    Add files via upload

--- pole.cpp
+++ pole.cpp
@@ -3,9 +3,14 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#define w 1000
-#define h 750
+#include <cmath>
+#include <iostream>
+
+#define w 1200
+#define h 900
 using namespace cv;
+using namespace std;
+
 
 void MyFilledCircle( Mat img, Point center, int bgr[], int size )
 {
@@ -17,26 +22,42 @@ void MyFilledCircle( Mat img, Point center, int bgr[], int size )
         LINE_8 );
 }
 
+void draw(Mat bckg, char maggytone[])
+{
+    int bgr[3]={0,0,255};
+    int size=2,r=22;
+    int i;
+    Point pt;
+    pt.x = w/2;
+    pt.y = h/2;
+
+
+    for (double i = 0; i < 6.3; i=i+0.001)
+    {
+        //size=rand()%2+2;
+
+        pt.x=(16*pow(sin(i),3))*r+w/2;
+        pt.y=-(13*cos(i)-5*cos(2*i)-2*cos(3*i)-cos(4*i))*r+h/2;
+        MyFilledCircle(bckg,pt,bgr,size);
+        moveWindow( maggytone, 0, 0 );
+        imshow( maggytone, bckg );
+        waitKey(1);
+
+        cout<<i<<endl;
+    }
+}
+
 int main( void )
 {
     char maggytone[] = "MaggyTone Project";
-    int bgr[3]={0,0,255};
-    int size=10;
 
-    Point pt;
     Mat bckg(h, w, CV_8UC3, Scalar(255, 255, 255));
-
-    pt.x = 0;
-    pt.y = 100;
-
-    for (size_t i = 0; i < 100; i++) {
-        MyFilledCircle(bckg,pt,bgr,size);
-        pt.x=i;
-    }
+    draw(bckg,maggytone);
 
 
-  imshow( maggytone, bckg );
-  moveWindow( maggytone, 0, 0 );
-  waitKey( 0 );
-  return(0);
+
+
+
+    waitKey( 0 );
+    return(0);
 }
